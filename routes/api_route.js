2cfa9756befb3b6f6facd3cabcc20fed6e053bf9
saboagye23 <85259473+saboagye23@users.noreply.github.com>
2021-09-18 21:52:49 -0700

    add post ap/notes action

--- routes/api_route.js
+++ routes/api_route.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
+const DB_FILE = 'db/db.json';
 
 module.exports = app => {
     let notes = [];
-    
+
     // read db.json file
-    fs.readFile('db/db.json','utf8', (err, data) => {
+    fs.readFile(DB_FILE,'utf8', (err, data) => {
         
         // throw an error if db file do not exit
         if(err) throw err;
@@ -14,8 +15,25 @@ module.exports = app => {
 
     });
 
+    const addNoteToDB = (note) => {
+        notes.push(note);
+        const db_json = JSON.stringify(notes, '\t');
+        fs.writeFile(DB_FILE, db_json, err =>{
+            if (err) throw err;
+            
+            console.log('New note added');
+            return true;
+        });
+    };
+
     app.get('/api/notes', (req, res) => {
         res.json(notes);
+    });
+
+    app.post('/api/notes', (req, res) => {
+        let note = req.body;
+        addNoteToDB(note);
+        res.json(note);
     });
 
 }
